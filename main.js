2b6b4b8bfede3aef7e1b4ad431e8fb2b192bea7f
Laura Benner <laurabennerr@gmail.com>
2023-06-14 15:20:18 -0400

    Time Filter

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 function run() {
-    var allRows = document.getElementsByTagName("tr");
-    var exhibitSort = document.getElementById("exhibit-select");
-    var exhibitSelected = exhibitSort.value;
-    for (row of allRows) {
-        var keep = row.className == exhibitSelected || row.className == "header" || exhibitSelected == "all";
-        row.style.display = keep ? "table-row" : "none";
+    let allRows = document.getElementsByTagName("tr");
+    let exhibitSort = document.getElementById("exhibit-select");
+    let exhibitSelected = exhibitSort.value;
+
+    let arrival = document.getElementById("arrival");
+    let arrivalTime = arrival.value;
+    let departure = document.getElementById("departure");
+    let departureTime = departure.value;
+    let allTimes = document.getElementsByClassName("time");
+
+    for (let i=1; i<allRows.length; i++) {
+        let time24 = allTimes[i-1].getAttribute("data-time");
+        let keep1 = departureTime > time24 && arrivalTime < time24;
+        let keep2 = allRows[i].className == exhibitSelected || exhibitSelected == "all";
+        allRows[i].style.display = keep1 && keep2 ? "table-row" : "none";
     }
-    var url = "url('images/" + exhibitSelected + ".jpg')";
-    console.log(url);
+    let url = "url('images/" + exhibitSelected + ".jpg')";
     document.body.style.backgroundImage = url;
-    docoument.body.style.backgroundRepeat = no-repeat;
+    document.body.style.backgroundRepeat = "no-repeat";
 }
 
